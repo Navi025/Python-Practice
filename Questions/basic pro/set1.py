e2f37f66b47f58c122f8bc62bbc7c83e24df9baa
Navi025 <20010001021@gateway.edu.in>
2024-03-30 23:16:38 +0530

    set 1

---
+++ Questions/basic pro/set1.py
@@ -0,0 +1,118 @@
+"""
+Q1. There are two variables.
+a=5
+b=10
+What will be the output of following:
+ - a > 5 and b >= 10
+ -  a >= 5 or not b > 10
+ - not ( a > 5 and b > 5)
+ - not ( a < 10 or not b < 10)
+ - not ( not a <= 5 or not b >= 10)
+"""
+# def new_func():
+#     a = 5
+#     b = 10
+#     print(a>5)
+#     print(a >= 5 or b<10)
+#     print(a>5 or b<10)
+# new_func()
+
+"""
+Q2. Python program to convert kilometers to miles. Ask kilometer from
+User.
+"""
+# def km_2_mi():
+#     mi=km*0.621371
+#     print(f"{km} km is equal to {mi} miles")
+
+# km=float(input("Enter distance in Kilometers:  "))
+# km_2_mi()
+
+"""
+Q3. Ask 3 numbers from User and Calculate the Average.
+"""
+
+# def avg():
+#     avg=(n1+n2+n3)/3
+#     print(f"The Average of {n1},{n2} & {n3} is {avg}")
+
+# n1=int(input("Enter I no: "))
+# n2=int(input("Enter II no: "))
+# n3=int(input("Enter III no: "))
+# avg()
+
+"""
+Q4. Ask value in Rupees and Convert into Paise.
+"""
+# def rs_2_ps():
+#     ps=rs*100
+#     print(f"Rs{rs} is equal to {ps} paise")
+
+# rs=int(input("Enter amount in INR: "))
+# rs_2_ps()
+
+"""
+Q5. Calculate Area of Square by taking side from User
+"""
+# def ar_sq():
+#     a=s*s
+#     print(f"Area of squate with side {s} is {a} sq units")
+
+# s=float(input("Enter side of the square: "))
+# ar_sq()
+
+"""
+Q6. Ask number of games played in a tournament. Ask the user number of
+games won and number of games loss. Calculate number of tie and total
+Points. (1 win= 4 points, 1 tie =2 points)
+"""
+# def tie():
+#     t=p-(l+w)
+#     print(f"Total tie matches: {t}")
+
+
+# def Tscore(t):
+#     ts=w*4+t*2
+#     print(f"Total score is {ts}")
+
+# p=int(input("Enter no of total games played: "))
+# w=int(input("Enter total no matches won: "))
+# l=int(input("Enter total no of matches loseL: "))
+# tie()
+# Tscore(p-(l+w))
+       
+"""
+Q7. Check if the number entered by User is divisible by 3 or not
+"""
+# def div3(n):
+#     if n%3==0:
+#         print(f"{n} is divisible by 3.")
+#     else:
+#         print(f"{n} is not divisible by 3.")
+# n=int(input("Enter the no: "))
+# div3(n)
+
+"""
+Q8. Ask a number from user. Print if the number is ODD or EVEN.
+"""
+# def od_ev(n):
+#     if n%2==0:
+#         print(f"{n} is EVEN")
+#     else:
+#         print(f"{n} is ODD")
+
+# n=int(input("Enter the no: "))
+# od_ev(n)
+
+"""
+Q9. Take values of length and breadth of a rectangle from user and check if
+it is square or not.
+"""
+def ck_sq(l,b):
+    if l==b:
+        print("It is a SQUARE")
+    else:
+        print("It is a RECTANGEL")
+l=float(input("Enter length: "))
+b=float(input("Enter breadth: "))
+ck_sq(l,b)
